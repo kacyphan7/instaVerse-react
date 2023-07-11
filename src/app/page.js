aec991c4eb71e2429f8839135e4840e3af66db24
Kacy Phan <kacyphann@gmail.com>
2023-07-10 18:38:19 -0700

    feat: add to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,30 @@
 import './globals.css';
 import './post.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faComment } from '@fortawesome/free-solid-svg-icons';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 export default function Homepage() {
+
   return (
     <main className="flex min-h-screen flex-col">
       <div className="page-content page-container" id="page-content">
         <div className="padding">
           <div className="row container d-flex justify-content-center">
-            <div className="col-md-6">
+            {Array.from({ length: 8 }, (_, index) => (
+              <div className="col-md-1" key={index}>
+                <div className="user-avatar">
+                  <FontAwesomeIcon icon={faUserCircle} className="user-icon bigger" />
+                </div>
+              </div>
+            ))}
+            <div className="row container d-flex justify-content-start">
               <div className="box box-widget">
-                <div className="box-header with-border">
-                  <div className="user-block">
+                <div className="box-header with-border ">
+                  <div className="user-block ">
+                    <br></br>
                     <img className="img-circle" src="https://img.icons8.com/color/36/000000/guest-male.png" alt="User Image" />
                     <span className="username">
                       <a href="#" data-abc="true">Henery German</a>
@@ -141,6 +151,11 @@ export default function Homepage() {
                       <input type="text" className="form-control input-sm" placeholder="Press enter to post comment" />
                     </div>
                   </form>
+                </div>
+              </div>
+              <div className="" >
+                <div className="row container d-flex justify-content-end">
+                  Suggested for you
                 </div>
               </div>
             </div>
@@ -151,3 +166,4 @@ export default function Homepage() {
   );
 }
 
+// style=“left: 1039.5px;”
