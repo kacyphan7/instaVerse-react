3aaf126380a7c055dfb85868cd0a48670d24d7ad
Kacy Phan <kacyphann@gmail.com>
2023-07-12 21:50:02 -0700

    finished styling home page w/o logic

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 // import Image from 'next/image';
 // import 'bootstrap/dist/css/bootstrap.css';
 //import './globals.css';
-//import './post.css';
+import './css/post.css';
 import './css/home.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
@@ -24,7 +24,7 @@ export default function Homepage() {
   // }
   setAuthToken(localStorage.getItem('jwtToken'));
   return (
-    <main className="d-flex justify-content-center align-items-center vh-100">
+    <main className="d-flex justify-content-center align-items-center">
       <div className="home-feed">
         <div className="suggestions">
           <ul>
@@ -165,37 +165,74 @@ export default function Homepage() {
             {/* Add more story users */}
           </div>
         </div>
-        <div className="card post">
-          <div className="card-body">
-            <div className="post-header">
-              <img
-                src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60"
-                alt="User Avatar"
-                className="post-avatar"
-              />
-              <h3>username</h3>
+
+        <div className="row container d-flex justify-content-center">
+          <div className="col-md-6">
+            <div className="box box-widget">
+              <div className="box-header with-border ">
+                <div className="user-block ">
+                  <br></br>
+                  <img className="img-circle" src="https://img.icons8.com/color/36/000000/guest-male.png" alt="User Image" />
+                  <span className="username">
+                    <a href="#" data-abc="true">Henery German</a>
+                  </span>
+                  <span className="description">Public - 7:30 PM Today</span>
+                </div>
+                <div className="box-tools">
+                  <button type="button" className="btn btn-box-tool" data-toggle="tooltip" title="" data-original-title="Mark as read">
+                    <i className="fa fa-circle-o"></i>
+                  </button>
+                  <button type="button" className="btn btn-box-tool" data-widget="collapse">
+                    <i className="fa fa-minus"></i>
+                  </button>
+                  <button type="button" className="btn btn-box-tool" data-widget="remove">
+                    <i className="fa fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <div className="box-body">
+                <img className="img-responsive pad" src="https://i.imgur.com/EAQkLS1.jpg" alt="Photo" />
+                <p>Look at the beach photo I clicked</p>
+                <button type="button" className="btn btn-default btn-xs">
+                  <FontAwesomeIcon icon={faHeart} />
+                </button>
+                <button type="button" className="btn btn-default btn-xs">
+                  <FontAwesomeIcon icon={faComment} />
+                </button>
+                <button type="button" className="btn btn-default btn-xs">
+                  <FontAwesomeIcon icon={faPaperPlane} />
+                </button>
+                <span className="pull-right text-muted">127 likes - 3 comments</span>
+              </div>
+              <div className="box-footer box-comments">
+                <div className="box-comment">
+                  <img className="img-circle img-sm" src="https://img.icons8.com/office/36/000000/person-female.png" alt="User Image" />
+                  <div className="comment-text">
+                    <span className="username">
+                      Tina Domiaz <span className="text-muted pull-right">8:03 PM Today</span>
+                    </span>
+                    For what reason would it be advisable for me to think about business content?
+                  </div>
+                </div>
+                <div className="box-comment">
+                  <img className="img-circle img-sm" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="User Image" />
+                  <div className="comment-text">
+                    <span className="username">
+                      Smith helm <span className="text-muted pull-right">8:03 PM Today</span>
+                    </span>
+                    That might be a little bit risky to have a crew member like them.
+                  </div>
+                </div>
+              </div>
+              <div className="box-footer">
+                <form action="#" method="post">
+                  <img className="img-responsive img-circle img-sm" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="Alt Text" />
+                  <div className="img-push">
+                    <input type="text" className="form-control input-sm" placeholder="Press enter to post comment" />
+                  </div>
+                </form>
+              </div>
             </div>
-            <img
-              src="https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
-              alt="Post"
-              className="post-image"
-            />
-            <p className="post-caption">caption</p>
-            <br />
-            <div className="post-header">
-              <img
-                src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60"
-                alt="User Avatar"
-                className="post-avatar"
-              />
-              <h3>username</h3>
-            </div>
-            <img
-              src="https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
-              alt="Post"
-              className="post-image"
-            />
-            <p className="post-caption">caption</p>
           </div>
         </div>
       </div>
