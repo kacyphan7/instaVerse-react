5fcaa6610ab540063927aae30feb126c77c29a27
Jay Lee <randomdori@gmail.com>
2023-07-14 23:26:30 -0700

    feat: update post page

--- src/app/comment/Comment.js
+++ src/app/comment/Comment.js
@@ -1,23 +1,64 @@
+'use client';
+import React, { useState, useEffect, use } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+
 import { faker } from "@faker-js/faker";
 export default function Comment({ comment }) {
+
+    const router = useRouter();
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/${comment.username}`)
+            .then((response) => {
+                // data is an object
+                setUserData(response.data.user);
+            })
+            .catch((error) => {
+                console.log('error', error);
+            });
+    }, [router, comment.username]);
+
+
+
+
     return (
-        <article className="media">
-            <figure className="media-left">
-                <p className="image is-48x48">
-                    <img src={faker.image.avatar()} />
-                </p>
-            </figure>
-            <div className="media-content">
-                <div className="content">
+        <div className="box-footer box-comments">
+            <div className="box-comment">
+
+                <div className="comment-text">
                     <p>
+                        <img src={userData.profilePicture} />
                         <strong>{comment.username}</strong>
                         <br />
                         {comment.comment}
                         <br />
                         {comment.likes}
                     </p>
+
                 </div>
             </div>
-        </article>
+        </div>
+        // <article className="media">
+        //     <figure className="media-left">
+        //         <p className="image is-48x48">
+        //             <img src={faker.image.avatar()} />
+        //         </p>
+        //     </figure>
+        //     <div className="media-content">
+        //         <div className="content">
+        //             <p>
+        //                 <img src={userData.profilePicture} />
+        //                 <strong>{comment.username}</strong>
+        //                 <br />
+        //                 {comment.comment}
+        //                 <br />
+        //                 {comment.likes}
+        //             </p>
+        //         </div>
+        //     </div>
+        // </article>
+
     );
 }
--- src/app/post/Post.js
+++ src/app/post/Post.js
@@ -1,92 +1,127 @@
+'use client';
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import axios from 'axios';
-import { useRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faComment, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import Comment from '../comment/new/page';
 import { faker } from '@faker-js/faker';
-import setAuthToken from './utils/setAuthToken';
+import jwtDecode from 'jwt-decode';
+import setAuthToken from '@/app/utils/setAuthToken';
+import moment from 'moment';
+import '../css/post.css';
 
 export default function Post({ post }) {
-    const rows = [];
+    const dateTimeAgo = moment(new Date(post.updatedAt)).fromNow();
+    const router = useRouter();
+    const [userData, setUserData] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+
+    const commentRows = [];
     if (post.comments.length) {
         post.comments.forEach((comment) => {
-            rows.push(<Comment comment={comment} key={comment._id} />);
+            commentRows.push(<Comment comment={comment} key={comment._id} />);
         });
-    }
+    } else { }
 
-    setAuthToken(localStorage.getItem('jwtToken'));
+    useEffect(() => {
+        setAuthToken(localStorage.getItem('jwtToken'));
+        if (localStorage.getItem('jwtToken')) {
+
+            axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/${localStorage.getItem('userId')}`)
+                .then((response) => {
+                    // data is an object
+                    let userData = jwtDecode(localStorage.getItem('jwtToken'));
+                    if (userData.email === localStorage.getItem('email')) {
+                        setUserData(response.data.user);
+                        setLoading(false);
+                    } else {
+                        console.log('error1', error);
+                        router.push('/users/login');
+
+                    }
+                })
+                .catch((error) => {
+                    console.log('error2', error);
+
+                    router.push('/users/login');
+
+                });
+
+        } else {
+            console.log('error3', error);
+            router.push('/users/login');
+
+        }
+    }, [router]);
+
+    if (isLoading) return <p>Loading...</p>;
+    if (!userData) return <p>No data shown...</p>;
+
+    // setAuthToken(localStorage.getItem('jwtToken'));
     return (
         <div className="row container d-flex justify-content-center">
-            <div>
-                {data.map((user, index) => (
-                    <div key={index}>
-                        <div className="box box-widget">
-                            <div className="box-header with-border ">
-                                <div className="user-block">
-                                    <br />
-                                    <img src={faker.image.avatar()} />
-                                    <span className="username">
-                                        <a href="#" data-abc="true">
-                                            {user.username}
-                                        </a>
-                                    </span>
-                                    <span className="description">Public - 7:30 PM Today</span>
-                                </div>
-                                <div className="box-tools">
-                                    <button type="button" className="btn btn-box-tool" data-toggle="tooltip" title="" data-original-title="Mark as read">
-                                        <i className="fa fa-circle-o"></i>
-                                    </button>
-                                    <button type="button" className="btn btn-box-tool" data-widget="collapse">
-                                        <i className="fa fa-minus"></i>
-                                    </button>
-                                    <button type="button" className="btn btn-box-tool" data-widget="remove">
-                                        <i className="fa fa-times"></i>
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="box-body">
-                                <img className="img-responsive pad" src={faker.image.url()} alt="Photo" />
-                                <p>Look at the beach photo I clicked</p>
-                                <button type="button" className="btn btn-default btn-xs">
-                                    <FontAwesomeIcon icon={faHeart} />
-                                </button>
-                                <button type="button" className="btn btn-default btn-xs">
-                                    <FontAwesomeIcon icon={faComment} />
-                                </button>
-                                <button type="button" className="btn btn-default btn-xs">
-                                    <FontAwesomeIcon icon={faPaperPlane} />
-                                </button>
-                                <span className="pull-right text-muted">127 likes - 3 comments</span>
-                            </div>
-                            <div className="box-footer box-comments">
-                                <div className="box-comment">
-                                    <img className="img-circle img-sm" src={faker.image.avatar()} alt="User Image" />
-                                    <div className="comment-text">
-                                        <span className="username">
-                                            {user.username}
-                                            <span className="text-muted pull-right">8:03 PM Today</span>
-                                        </span>
-                                        For what reason would it be advisable for me to think about business content?
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="box-footer">
-                                <form action="#" method="post">
-                                    <img
-                                        className="img-responsive img-circle img-sm"
-                                        src={data.profilePicture || 'https://freesvg.org/img/abstract-user-flat-4.png'}
-                                        alt="Alt Text"
-                                    />
-                                    <div className="img-push">
-                                        <input type="text" className="form-control input-sm" placeholder="Press enter to post comment" />
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
+
+            {/* {data.map((user, index) => ( */}
+            {/* <div key={index}> */}
+
+            <div className="box box-widget">
+                <div className="box-header with-border ">
+                    <div className="user-block">
+                        <br />
+                        <img src={userData.profilePicture} />
+                        <span className="username">
+                            <a href="#" data-abc="true">
+                                {/* {user.username} */}
+                            </a>
+                        </span>
+                        <span className="description">Public - {dateTimeAgo}</span>
                     </div>
-                ))}
+                    <div className="box-tools">
+                        <button type="button" className="btn btn-box-tool" data-toggle="tooltip" title="" data-original-title="Mark as read">
+                            <i className="fa fa-circle-o"></i>
+                        </button>
+                        <button type="button" className="btn btn-box-tool" data-widget="collapse">
+                            <i className="fa fa-minus"></i>
+                        </button>
+                        <button type="button" className="btn btn-box-tool" data-widget="remove">
+                            <i className="fa fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div className="box-body">
+                    <img className="img-responsive pad" src={post.photo} alt="Photo" />
+                    <p className='caption'>{post.caption}</p>
+                    <button type="button" className="btn btn-default btn-xs">
+                        <FontAwesomeIcon icon={faHeart} />
+                    </button>
+                    <button type="button" className="btn btn-default btn-xs">
+                        <FontAwesomeIcon icon={faComment} />
+                    </button>
+                    <button type="button" className="btn btn-default btn-xs">
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </button>
+                    <span className="pull-right text-muted">{post.likes} likes - {post.comments.length} comments</span>
+                </div>
+                {commentRows}
+
+                <div className="box-footer">
+                    <form action="#" method="post">
+                        <img
+                            className="img-responsive img-circle img-sm"
+                            src={userData.profilePicture || 'https://freesvg.org/img/abstract-user-flat-4.png'}
+                            alt="Alt Text"
+                        />
+                        <div className="img-push">
+                            <input type="text" className="form-control input-sm" placeholder="Press enter to post comment" />
+                        </div>
+                    </form>
+                </div>
             </div>
+
+            {/* ))} */}
+
         </div>
     );
 }
+
