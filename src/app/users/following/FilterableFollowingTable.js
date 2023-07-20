6e2689e84c59ac92b31d5cd80844f7d389f3f1b7
Jay Lee <randomdori@gmail.com>
2023-07-19 20:14:05 -0700

    chore: try fixing localstorage issue

--- src/app/users/following/FilterableFollowingTable.js
+++ src/app/users/following/FilterableFollowingTable.js
@@ -17,15 +17,15 @@ export default function FilterableFollowingTable() {
     if (typeof window !== 'undefined' && localStorage.getItem('jwtToken')) {
         const expirationTime = new Date(parseInt(localStorage.getItem('expiration')) * 1000);
         let currentTime = Date.now();
-    }
 
-    // make a condition that compares exp and current time
-    if (currentTime >= expirationTime) {
-        handleLogout();
-        alert('Session has ended. Please login to continue.');
-        router.push('/users/login');
-    }
 
+        // make a condition that compares exp and current time
+        if (currentTime >= expirationTime) {
+            handleLogout();
+            alert('Session has ended. Please login to continue.');
+            router.push('/users/login');
+        }
+    }
     useEffect(() => {
         if (typeof window !== 'undefined' && localStorage.getItem('jwtToken')) {
             const expirationTime = new Date(parseInt(localStorage.getItem('expiration')) * 1000);
